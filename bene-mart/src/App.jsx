c3049a4c090c28a45465b0b708aa18bdca9d13f5
Ayush Kumar <87794509+rayush158@users.noreply.github.com>
2022-03-28 19:38:28 +0530

    Initial Commit

---
+++ bene-mart/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./logo.png";
+import { Home } from "./Pages/Home";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ bene-mart/src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../Components/Header/Header";
+export const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
