0a849e4e9cc81654b307bb760ec4d006d0c4fbc1
Ayush Kumar <87794509+rayush158@users.noreply.github.com>
2022-04-04 15:50:57 +0530

    fix: update Category items

--- bene-mart/src/backend/db/categories.js
+++ bene-mart/src/backend/db/categories.js
@@ -8,20 +8,44 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    categoryName: "Gaming PCs",
+    image: "https://promotions.newegg.com/international/21-1780/pcs.png",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    categoryName: "Gaming Laptops",
+    image: "https://promotions.newegg.com/international/21-1780/laptop2.png",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryName: "Motherboards",
+    image:
+      "https://promotions.newegg.com/international/21-1780/motherboard1.png",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Video Cards",
+    image:
+      "https://promotions.newegg.com/international/21-1780/graphiccard2.png",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Storage",
+    image: "https://promotions.newegg.com/international/21-1780/storage1.png",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Memory",
+    image: "https://promotions.newegg.com/international/21-1780/memory.png",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Monitors",
+    image: "https://promotions.newegg.com/international/21-1780/monitor.png",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Cases",
+    image: "https://promotions.newegg.com/international/21-1780/cases.png",
   },
 ];
